275df384415895effed951f9e30d5f089c0868b5
maruchronos <yokormsm>
2018-12-02 16:02:07 -0300

    :rocket: Create route config

--- app/module.js
+++ app/module.js
@@ -1,7 +1,31 @@
 const app = angular.module("app", [
     'ngMaterial', 
     'chartCardModule',
-    'bars'
+    'bars',
+    'ngRoute'
 ]);
 
 app.controller('appController', function ($scope) { });
+
+app.config(function config($routeProvider, $locationProvider) {
+    $routeProvider.
+        when('/', {
+        template: `
+            <chart-card
+                title="Doughnut Chart"
+                description="This is a sample of a Donut chart inside an Agular 1.5 app using Angular Material">
+            </chart-card>
+            <bar-chart
+                title="Bars Chart"
+                description="Now, this is a sample of a Bars chart. Also using angular-chart, a wrapper to Chart.js">
+            </bar-chart>`
+        }).
+        when('/charts', {
+        templateUrl: '/pages/charts.html'
+        }).
+        otherwise('/');
+    $locationProvider.html5Mode({
+        enabled: true
+    });
+
+});
