d794b8d6463a4648e0bb2c45e9435071159766b8
maruchronos <yokormsm>
2018-12-05 22:28:34 -0300

    :rocket: Change navigation to Ui Router

--- app/module.js
+++ app/module.js
@@ -1,49 +1,55 @@
 const app = angular.module("app", [
-    'ngMaterial',
-    'firebase',
-    'headerModule',
-    'sidebarModule',
-    'chartCardModule',
-    'bars',
-    'ngRoute'
+  'ngMaterial',
+  'firebase',
+  'headerModule',
+  'sidebarModule',
+  'chartCardModule',
+  'bars',
+  'ui.router'
 ]);
 
 app.controller('appController', () => {});
 
-app.config(function config($routeProvider, $locationProvider) {
-    $routeProvider.
-        when('/', {
-        template: `
-            <chart-card
-                title="Doughnut Chart">
-            </chart-card>
-            <bar-chart
-                title="Bars Chart"
-                description="
-                    Now, this is a sample of a Bars chart. Also using angular-chart and Angular Material. In this case I'm using a static reference from
-                    firebase, like consuming an API, so real-time changes will not be noticed.
-                ">
-            </bar-chart>`
-        }).
-        when('/donut', {
-        template: `
-            <chart-card
-                title="Doughnut Chart">
-            </chart-card>`
-        }).
-        when('/bars', {
-        template: `
-        <bar-chart
-            title="Bars Chart"
-            description="
-                Now, this is a sample of a Bars chart. Also using angular-chart and Angular Material. In this case I'm using a static reference from
-                firebase, like consuming an API, so real-time changes will not be noticed.
-            ">
-        </bar-chart>
-        `
-        });
-    // $locationProvider.html5Mode({
-    //     enabled: true
-    // });
+app.config(function config($stateProvider) {
+  const HOME = {
+    name: 'home',
+    url: '/',
+    template: `
+      <chart-card
+          title="Doughnut Chart">
+      </chart-card>
+      <bar-chart
+          title="Bars Chart"
+          description="
+              Now, this is a sample of a Bars chart. Also using angular-chart and Angular Material. In this case I'm using a static reference from
+              firebase, like consuming an API, so real-time changes will not be noticed.
+          ">
+      </bar-chart>`
+  };
 
+  const DONUT = {
+    name: 'donut',
+    url: '/donut',
+    template: `
+      <chart-card
+          title="Doughnut Chart">
+      </chart-card>`
+  };
+
+  const BARS = {
+    name: 'bars',
+    url: '/bars',
+    template: `
+      <bar-chart
+        title="Bars Chart"
+        description="
+          Now, this is a sample of a Bars chart. Also using angular-chart and Angular Material. In this case I'm using a static reference from
+          firebase, like consuming an API, so real-time changes will not be noticed.
+        ">
+      </bar-chart>`
+  };
+
+  $stateProvider.state(HOME);
+  $stateProvider.state(DONUT);
+  $stateProvider.state(BARS);
 });
